1a2eb7d3c438f4b33051512d538d7d25ec2f6a58
Romain Pomier <romain@cardiologs.com>
2016-07-26 14:50:39 +0200

    Better association copy

--- lib/forceps/client.rb
+++ lib/forceps/client.rb
@@ -119,7 +119,9 @@ module Forceps
     end
 
     def make_associations_reference_remote_classes_for(model_class)
-      model_class.reflect_on_all_associations.each do |association|
+      model_class.reflect_on_all_associations
+      .map { |a| model_class._reflect_on_association(a.name.to_s) }
+      .each do |association|
         next if association.class_name =~ /Forceps::Remote/ rescue next
         reference_remote_class(model_class, association)
       end
